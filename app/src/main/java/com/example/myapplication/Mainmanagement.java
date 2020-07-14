0b683b2755ffe1d73ddc58b694418a0c775ae711
dahye <dahye0729@sch.ac.kr>
2020-07-14 14:17:55 +0900

    사진 추가하고싶은데...

--- app/src/main/java/com/example/myapplication/Mainmanagement.java
+++ app/src/main/java/com/example/myapplication/Mainmanagement.java
@@ -1,13 +1,43 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Mainmanagement extends AppCompatActivity {
+    EditText mname;
+    EditText mnum;
+    Button mcheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_mainmanage);
+        mname = (EditText) findViewById(R.id.managername);
+        mnum = (EditText) findViewById(R.id.managernumber);
+        mcheck = (Button) findViewById(R.id.managerloginbtn);
+
+        mname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mnum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/myapplication/Mainmember.java
+++ app/src/main/java/com/example/myapplication/Mainmember.java
@@ -3,12 +3,40 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Mainmember extends AppCompatActivity {
+    EditText memname;
+    EditText memnum;
+    Button memcheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmember);
+        memname = (EditText) findViewById(R.id.memname);
+        memnum = (EditText) findViewById(R.id.memnumber);
+        memcheck = (Button)findViewById(R.id.memberloginbtn);
+
+        memname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        memnum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        memcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
