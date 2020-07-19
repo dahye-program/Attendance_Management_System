0b95df1aa655dcb5c5689eddbd53676cf4532e03
dahye-program <dahye0729@sch.ac.kr>
2020-07-19 23:33:00 +0900

    QR코드 버튼 클릭

--- app/src/main/java/com/example/myapplication/member_generate_qrcode.java
+++ app/src/main/java/com/example/myapplication/member_generate_qrcode.java
@@ -3,12 +3,30 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class member_generate_qrcode extends AppCompatActivity {
-
+    Button memAttendancebtn;
+    Button memLeaveworkbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_generate_qrcode);
+        memAttendancebtn = (Button)findViewById(R.id.memattendance);
+        memLeaveworkbtn = (Button)findViewById(R.id.memLeavework);
+
+        memAttendancebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        memLeaveworkbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
