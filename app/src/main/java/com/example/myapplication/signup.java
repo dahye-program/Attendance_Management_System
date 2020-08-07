671278870bb72b88fc22b70355a5aead277ce84b
dahye <dahye0729@sch.ac.kr>
2020-08-07 11:30:49 +0900

    add signup

--- app/src/main/java/com/example/myapplication/signup.java
+++ app/src/main/java/com/example/myapplication/signup.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -13,34 +14,42 @@ public class signup extends AppCompatActivity {
     RadioButton managebtn;
     RadioButton membtn;
     RadioGroup radioGroup;
+    Button check;
+    String signupResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
+        // 라디오 버튼 설정
         managebtn = (RadioButton)findViewById(R.id.signupmanager);
-        managebtn.setOnClickListener(radioButtonClickListener);
-
         membtn = (RadioButton)findViewById(R.id.signupmember);
-        membtn.setOnClickListener(radioButtonClickListener);
-
+        // 라디오 그룹 설정
         radioGroup = (RadioGroup)findViewById(R.id.radioGroup);
-        radioGroup.setOnCheckedChangeListener(radioGroupButtonChangeListener);
-    }
-    RadioButton.OnClickListener radioButtonClickListener = new RadioButton.OnClickListener(){
-        @Override
-        public void onClick(View view){
-            Toast.makeText(this,"managebtn : "+managebtn.isChecked()+"membtn:"+membtn.isChecked(),Toast.LENGTH_SHORT).show();
-        }
-    };
-    RadioGroup.OnCheckedChangeListener radioGroupButtonChangeListener = new RadioGroup.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(RadioGroup radioGroup, int i) {
-            if(i==R.id.signupmanager){
-                Toast.makeText(this,"관리자!!",Toast.LENGTH_SHORT).show();
+        check=(Button)findViewById(R.id.signupcheckbtn);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i==R.id.signupmanager){
+                    Toast.makeText(signup.this,"관리자 입니다.",Toast.LENGTH_SHORT).show();
+                    signupResult=managebtn.getText().toString();
+                }else if(i==R.id.signupmember){
+                    Toast.makeText(signup.this, "회원 입니다.", Toast.LENGTH_SHORT).show();
+                    signupResult=membtn.getText().toString();
+                }
             }
-        }
+        });
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(signupResult!=null){
+                    Toast.makeText(signup.this,signupResult+" 회원가입", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(signup.this,"관리자/회원 선택 해주세요",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 
 }
